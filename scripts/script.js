7c94edfe0f1673ef9c80c099e69f047cd3b054fc
arty gvozdenkov <gvozdenkov@gmail.com>
2022-11-04 05:52:41 +0300

    refact: edit profile & add card popup clean code

--- scripts/script.js
+++ scripts/script.js
@@ -14,59 +14,64 @@ let profileName = content.querySelector(".profile__name");
 let profileAbout = content.querySelector(".profile__about");
 
 // ============== Popup functions =============================================
-const handleClosePopup = (evt) => {
-  if (evt.target.classList.contains("popup")) closePopup(evt.target);
-};
-
-const handlePopupOpen = (popup) => {
+const openPopup = (popup) => {
   popup.classList.add("popup_opened");
-  const closePopupButton = popup.querySelector(".popup__close-btn");
-  closePopupButton.addEventListener("click", () => closePopup(popup));
-  popup.addEventListener("mousedown", handleClosePopup);
 };
 
 const closePopup = (popup) => {
   popup.classList.remove("popup_opened");
-  popup.removeEventListener("mousedown", handleClosePopup);
 };
 
-const getFormInputValues = (inputList) => {
-  return Array.from(inputList).reduce(
+const setPopupCloseListeners = (popup) => {
+  const closePopupButton = popup.querySelector(".popup__close-btn");
+  closePopupButton.addEventListener("click", () => closePopup(popup));
+};
+
+const getFormInputValues = (form) => {
+  return Array.from(form.elements).reduce(
     (acc, input) => ({ ...acc, [input.name]: input.value }),
     {}
   );
 };
 
 const cleanFormInputs = (form) => {
-  [...form.elements].forEach((input) => (input.value = ""));
+  [...form.elements].forEach((input) => {
+    input.nodeName === "INPUT" ? (input.value = "") : null;
+  });
 };
 
 //=============== Form functions ==============================================
 const handleEditProfileSubmit = (evt) => {
   evt.preventDefault();
-
-  const inputList = evt.target.querySelectorAll(".form__input");
-  const { name, about } = getFormInputValues(inputList);
+  const form = evt.target;
+  const { name, about } = getFormInputValues(form);
   profileName.textContent = name;
   profileAbout.textContent = about;
-  cleanFormInputs(evt.target);
-  closePopup(evt.target.closest(".popup"));
+  closePopup(form.closest(".popup"));
 };
 
-const fillEditProfileFormInputs = () => {
-  const nameInput = editProfileForm.querySelector(".form__input_type_name");
-  const aboutInput = editProfileForm.querySelector(".form__input_type_about");
-  nameInput.value = profileName.textContent;
-  aboutInput.value = profileAbout.textContent;
+const handleAddCardSubmit = (evt) => {
+  evt.preventDefault();
+  const form = evt.target;
+  cleanFormInputs(form);
+  closePopup(form.closest(".popup"));
 };
 
 // ============== form submit listeners =======================================
 editProfileForm.addEventListener("submit", handleEditProfileSubmit);
+addCardForm.addEventListener("submit", handleAddCardSubmit);
 
-// ============== button listeners =============================================
+// ============== popup button listeners ======================================
 editProfileButton.addEventListener("click", () => {
-  handlePopupOpen(editProfilePopup);
-  fillEditProfileFormInputs();
+  setPopupCloseListeners(editProfilePopup);
+  openPopup(editProfilePopup);
+  const nameInput = editProfileForm.querySelector(".form__input_type_name");
+  const aboutInput = editProfileForm.querySelector(".form__input_type_about");
+  nameInput.value = profileName.textContent;
+  aboutInput.value = profileAbout.textContent;
 });
 
-addCardButton.addEventListener("click", () => handlePopupOpen(addCardPopup));
+addCardButton.addEventListener("click", () => {
+  setPopupCloseListeners(addCardPopup);
+  openPopup(addCardPopup);
+});
