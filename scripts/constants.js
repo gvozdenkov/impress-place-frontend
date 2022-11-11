14fc7de37b0344efa4b11408f31b0070cc360c3c
arty gvozdenkov <gvozdenkov@gmail.com>
2022-11-11 10:43:32 +0300

    refact: move fake DB in separate js file

---
+++ scripts/constants.js
@@ -0,0 +1,26 @@
+const initialCards = [
+  {
+    name: "Алтайский край",
+    link: "https://images.unsplash.com/photo-1590933582783-be57939a1e7e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=626&q=80",
+  },
+  {
+    name: "Рыбалка на Волге",
+    link: "https://images.unsplash.com/photo-1623490823615-9d403523b019?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+  },
+  {
+    name: "Крыши Питера",
+    link: "https://images.unsplash.com/photo-1633211249429-1538a4b7ba9e?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=735&q=80",
+  },
+  {
+    name: "Камчатка",
+    link: "https://images.unsplash.com/photo-1584315565803-fe062aff262d?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1375&q=80",
+  },
+  {
+    name: "Сочи",
+    link: "https://images.unsplash.com/photo-1515578706925-0dc1a7bfc8cb?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=627&q=80",
+  },
+  {
+    name: "Байкал",
+    link: "https://images.unsplash.com/photo-1490879112094-281fea0883dc?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1469&q=80",
+  },
+];
